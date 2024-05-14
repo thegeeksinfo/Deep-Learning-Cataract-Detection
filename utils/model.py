e5bd4eafea8a8bcc007587ba090c5e0e116fe541
Dejezi <deguzmanernesto17@gmail.com>
2024-05-14 12:21:53 +0300

    Update model.py

--- utils/model.py
+++ utils/model.py
@@ -1,6 +1,38 @@
-from keras.models import Model
-from keras.layers import Flatten, Dense, Dropout, BatchNormalization, Activation, SpatialDropout2D
-from keras.applications import ResNet50
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Flatten, Dropout, BatchNormalization, Activation, SpatialDropout2D
+from tensorflow.keras.models import Model
+
+train_dir = '../data/train/'
+test_dir = '../data/test/'
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='binary'
+)
+
+test_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='binary'
+)
 
 # Loaded pre-trained ResNet50 model with the top (fully connected) layers included
 base_model = ResNet50(weights='imagenet', include_top=True, input_shape=(224, 224, 3))
@@ -22,6 +54,12 @@ model = Model(inputs=base_model.input, outputs=predictions)
 
 # Compile the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-# Print model summary
-model.summary()
+  
+# Train model
+model.fit(
+    train_generator,
+    steps_per_epoch=len(train_generator),
+    epochs=3,
+    validation_data=test_generator,
+    validation_steps=len(test_generator)
+)
